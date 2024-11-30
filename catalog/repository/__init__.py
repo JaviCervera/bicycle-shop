db9551c9b0851c293659a6c834824c86c47db0ce
Javier San Juan <jedive@gmail.com>
2024-11-30 08:38:52 +0100

    fix: Removed ruff warnings

--- catalog/repository/__init__.py
+++ catalog/repository/__init__.py
@@ -2,3 +2,10 @@ from .part_option_repository import PartOptionRepository
 from .product_model_repository import ProductModelRepository
 from .product_part_repository import ProductPartRepository
 from .product_repository import ProductRepository
+
+__all__ = [
+  'PartOptionRepository',
+  'ProductModelRepository',
+  'ProductPartRepository',
+  'ProductRepository',
+]
